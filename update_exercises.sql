c1dfca2a457fc60e2d05dd03037cf21762248808
CarsonBelew <carbelew@gmail.com>
2019-06-04 11:10:18 -0500

    cleaned up update code

--- update_exercises.sql
+++ update_exercises.sql
@@ -10,12 +10,12 @@ set sales = sales * 10;
 
 select * from albums where release_date < 1980;
 update albums
-set release_date = 1880
+set release_date = release_date - 100
 where release_date < 1980;
 
 
 select * from albums where artist = 'Michael Jackson';
-update  albums
+update albums
 set artist = 'Peter Jackson'
 where artist = 'Michael Jackson';
 
