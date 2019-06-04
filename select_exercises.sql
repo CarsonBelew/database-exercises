0fe590c0b37295078eea10d80c259e09430ea93a
CarsonBelew <carbelew@gmail.com>
2019-06-04 10:32:00 -0500

    changed between date

--- select_exercises.sql
+++ select_exercises.sql
@@ -3,6 +3,6 @@ USE codeup_test_db;
 SELECT record_name FROM albums where artist = 'Pink Floyd';
 SELECT release_date FROM albums where record_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 SELECT genre FROM albums where record_name = 'Nevermind';
-select * from albums where release_date between 1990 and 2000;
+select * from albums where release_date between 1990 and 1999;
 select * from albums where sales < 20;
 select * from albums where genre = 'rock';
