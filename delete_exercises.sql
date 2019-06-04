dd48467f3f4a6f6c5b2224b7812ad819a6ab0ea1
CarsonBelew <carbelew@gmail.com>
2019-06-04 11:43:22 -0500

    finished exercise

---
+++ delete_exercises.sql
@@ -0,0 +1,26 @@
+USE codeup_test_db;
+
+SELECT record_name
+  AS 'Albums released after 1991'
+FROM albums
+WHERE release_date > 1991;
+DELETE FROM albums
+WHERE release_date > 1991;
+
+
+SELECT record_name
+  AS 'Albums with the genre of disco'
+FROM albums
+WHERE genre = 'disco';
+DELETE FROM albums
+WHERE genre = 'disco';
+
+
+SELECT record_name
+  AS 'Albums done by Whitney Houston'
+FROM albums
+WHERE artist = 'Whitney Houston';
+DELETE FROM albums
+WHERE artist = 'Whitney Houston';
+
+
