296aa4a7be679af32ea68cfc2baf642329f8f643
CarsonBelew <carbelew@gmail.com>
2019-06-04 14:26:52 -0500

    Find all employees whose last name starts or ends with 'E' — 30,723 rows.

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,8 +2,5 @@ USE employees;
 
 SELECT emp_no, first_name, last_name, gender
 FROM employees
-WHERE gender = 'M'
-  AND
-  (first_name = 'Irena'
-OR first_name = 'Vidya'
-OR first_name = 'Maya');
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
