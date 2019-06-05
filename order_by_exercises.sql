a1447fae5382dd97ee1591fc30498d096bab44ee
CarsonBelew <carbelew@gmail.com>
2019-06-05 09:35:37 -0500

    Now reverse the sort order for both queries.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -16,3 +16,14 @@ WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%' ORDER BY emp_no;
+
+
+# Now reverse the sort order for both queries.
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_name;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' ORDER BY emp_no DESC ;
