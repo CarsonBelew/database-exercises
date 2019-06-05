2a3b340e29656ceb30ceb3066e5d14a521ecbd42
CarsonBelew <carbelew@gmail.com>
2019-06-05 09:30:19 -0500

    Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,3 +3,8 @@ USE employees;
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
