b9d64cae300ce94f48f9ac623b2652f422e7098a
CarsonBelew <carbelew@gmail.com>
2019-06-05 09:40:08 -0500

    Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -27,3 +27,12 @@ WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_nam
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%' ORDER BY emp_no DESC ;
+
+# Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+
+SELECT emp_no, first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date
+LIKE '199%'
+AND birth_date
+LIKE '%-12-25' ORDER BY birth_date ASC, hire_date DESC;
